ce08effc6ffb4b9c56dd5b23f52852fdc35650a6
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-20 21:36:32 +0200

    Initial commit

---
+++ src/test/java/com/example/bookido/BookidoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+
+public class BookidoOnlineApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
