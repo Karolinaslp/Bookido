316b10020b87e711f472a8782e73b83ecd52c165
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-21 14:09:45 +0200

    create BookidoOnlineApplication class

---
+++ src/main/java/com/example/bookido/BookidoOnlineApplication.java
@@ -0,0 +1,11 @@
+package com.example.bookido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookidoOnlineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookidoOnlineApplication.class, args);
+    }
+}
