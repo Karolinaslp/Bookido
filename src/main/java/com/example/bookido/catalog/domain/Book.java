de6eb05cb25d62bfa7cfa811f033ce4293b8943d
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-21 14:08:43 +0200

    declare and implement Book class

---
+++ src/main/java/com/example/bookido/catalog/domain/Book.java
@@ -0,0 +1,26 @@
+package com.example.bookido.catalog.domain;
+
+public class Book {
+    Long id;
+    String title;
+    String author;
+    Integer year;
+
+    public Book(Long id, String title, String author, Integer year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
+
