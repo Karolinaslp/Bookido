4c3d3f8e64a561d2630946a6c18f303fcae5b409
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-21 14:11:47 +0200

    create CatalogService class

---
+++ src/main/java/com/example/bookido/catalog/domain/CatalogService.java
@@ -0,0 +1,25 @@
+package com.example.bookido.catalog.domain;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CatalogService {
+
+    private final CatalogRepository repository;
+
+    public CatalogService(CatalogRepository repository) {
+        this.repository = repository;
+
+    }
+
+    public List<Book> findByTitle(String title) {
+        return repository.findAll()
+                .stream()
+                .filter(book -> book.title.startsWith(title))
+                .collect(Collectors.toList());
+    }
+}
+
