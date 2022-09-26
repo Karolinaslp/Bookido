69ca125a28e13350b7d04f266f0837c4486e0448
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-26 19:52:44 +0200

    add lombok annotation

--- src/main/java/com/example/bookido/ApplicationStartup.java
+++ src/main/java/com/example/bookido/ApplicationStartup.java
@@ -2,22 +2,20 @@ package com.example.bookido;
 
 import com.example.bookido.catalog.application.CatalogController;
 import com.example.bookido.catalog.domain.Book;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ApplicationStartup implements CommandLineRunner {
     private final CatalogController catalogController;
 
-    public ApplicationStartup(CatalogController catalogController) {
-        this.catalogController = catalogController;
-    }
-
     @Override
     public void run(String... args) {
-        List<Book> bookList = catalogController.findByTitle("Pan");
+        List<Book> bookList = catalogController.findByTitle("Java");
         bookList.forEach(System.out::println);
     }
 }
