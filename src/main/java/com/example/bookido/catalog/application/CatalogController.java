608e0aca363254e65ced102d19abfc52992f317c
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-26 19:53:05 +0200

    add lombok annotation

--- src/main/java/com/example/bookido/catalog/application/CatalogController.java
+++ src/main/java/com/example/bookido/catalog/application/CatalogController.java
@@ -2,17 +2,15 @@ package com.example.bookido.catalog.application;
 
 import com.example.bookido.catalog.domain.Book;
 import com.example.bookido.catalog.domain.CatalogService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class CatalogController {
     private final CatalogService service;
-
-    public CatalogController(CatalogService service) {
-        this.service = service;
-    }
 
     public List<Book> findByTitle(String title) {
         return service.findByTitle(title);
