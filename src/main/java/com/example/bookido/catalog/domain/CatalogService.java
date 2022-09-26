ffda8634fea685d8af9707fdd39ed8c16632ea2f
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-26 19:53:11 +0200

    add lombok annotation

--- src/main/java/com/example/bookido/catalog/domain/CatalogService.java
+++ src/main/java/com/example/bookido/catalog/domain/CatalogService.java
@@ -1,24 +1,20 @@
 package com.example.bookido.catalog.domain;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class CatalogService {
-
     private final CatalogRepository repository;
-
-    public CatalogService(CatalogRepository repository) {
-        this.repository = repository;
-
-    }
 
     public List<Book> findByTitle(String title) {
         return repository.findAll()
                 .stream()
-                .filter(book -> book.title.startsWith(title))
+                .filter(book -> book.getTitle().startsWith(title))
                 .collect(Collectors.toList());
     }
 }
