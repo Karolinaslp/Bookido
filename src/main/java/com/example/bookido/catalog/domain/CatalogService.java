b741495f740fd2033285b10b89f0ddf7a37206f4
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-26 21:18:31 +0200

    solving a double bean conflict in Spring

--- src/main/java/com/example/bookido/catalog/domain/CatalogService.java
+++ src/main/java/com/example/bookido/catalog/domain/CatalogService.java
@@ -1,15 +1,19 @@
 package com.example.bookido.catalog.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 public class CatalogService {
     private final CatalogRepository repository;
+
+    public CatalogService(@Qualifier("bestsellerCatalogRepository") CatalogRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Book> findByTitle(String title) {
         return repository.findAll()
