a884fed22d7613423ffd1663914d4f2724f2f585
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-26 19:52:56 +0200

    add lombok annotations

--- src/main/java/com/example/bookido/catalog/domain/Book.java
+++ src/main/java/com/example/bookido/catalog/domain/Book.java
@@ -1,26 +1,16 @@
 package com.example.bookido.catalog.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
 public class Book {
     Long id;
     String title;
     String author;
     Integer year;
-
-    public Book(Long id, String title, String author, Integer year) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", year=" + year +
-                '}';
-    }
 }
 
