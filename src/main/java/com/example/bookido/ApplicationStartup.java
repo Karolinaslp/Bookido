e194b0f1048ccae83e32815f5bf3d70ef1448557
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-27 20:20:38 +0200

    primitive value injection/ broken

--- src/main/java/com/example/bookido/ApplicationStartup.java
+++ src/main/java/com/example/bookido/ApplicationStartup.java
@@ -3,19 +3,29 @@ package com.example.bookido;
 import com.example.bookido.catalog.application.CatalogController;
 import com.example.bookido.catalog.domain.Book;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
+
 public class ApplicationStartup implements CommandLineRunner {
     private final CatalogController catalogController;
+    private final String title;
+
+    public ApplicationStartup(
+            CatalogController catalogController,
+            @Value("${bookido.catalog.query:Java}") String title
+    ) {
+        this.catalogController = catalogController;
+        this.title = title;
+    }
 
     @Override
     public void run(String... args) {
-        List<Book> bookList = catalogController.findByTitle("Java");
+        List<Book> bookList = catalogController.findByTitle(title);
         bookList.forEach(System.out::println);
     }
 }
