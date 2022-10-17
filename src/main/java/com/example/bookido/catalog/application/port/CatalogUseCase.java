85513edcbcfe2541731c13e862d5286593d19bbe
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-10-17 14:17:03 +0200

    create interface to CatalogService crud methods

---
+++ src/main/java/com/example/bookido/catalog/application/port/CatalogUseCase.java
@@ -0,0 +1,20 @@
+package com.example.bookido.catalog.application.port;
+
+import com.example.bookido.catalog.domain.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CatalogUseCase {
+    List<Book> findByTitle(String title);
+
+    List<Book> findAll();
+
+    Optional<Book> findOneByTitleAndAuthor(String title, String author);
+
+    void addBook();
+
+    void removeById(Long id);
+
+    void updateBook();
+}
