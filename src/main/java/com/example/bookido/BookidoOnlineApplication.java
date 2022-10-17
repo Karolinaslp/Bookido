d6d00e134ab707e482a18b2b48a7af4e6a5ab78b
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-10-17 14:14:47 +0200

    refactor

--- src/main/java/com/example/bookido/BookidoOnlineApplication.java
+++ src/main/java/com/example/bookido/BookidoOnlineApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookidoOnlineApplication {
+public class    BookidoOnlineApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookidoOnlineApplication.class, args);
     }
