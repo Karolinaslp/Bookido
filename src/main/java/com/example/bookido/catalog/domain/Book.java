19b0df2b8fbfdcbed4b92ab5be05c736c0cfa8bc
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-10-18 13:57:41 +0200

    add setter loombok annotation

--- src/main/java/com/example/bookido/catalog/domain/Book.java
+++ src/main/java/com/example/bookido/catalog/domain/Book.java
@@ -2,15 +2,23 @@ package com.example.bookido.catalog.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @ToString
 public class Book {
-    Long id;
-    String title;
-    String author;
-    Integer year;
+  private Long id;
+  private String title;
+  private String author;
+  private Integer year;
+
+  public Book(String title, String author, Integer year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
 }
 
