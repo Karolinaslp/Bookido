2186e0a14c47c3cfc582a04c5ea122666a40939c
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-10-19 11:10:28 +0200

    add UpdateBookResponse inner class

--- src/main/java/com/example/bookido/catalog/application/port/CatalogUseCase.java
+++ src/main/java/com/example/bookido/catalog/application/port/CatalogUseCase.java
@@ -3,8 +3,11 @@ package com.example.bookido.catalog.application.port;
 import com.example.bookido.catalog.domain.Book;
 import lombok.Value;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+
+import static java.util.Collections.*;
 
 public interface CatalogUseCase {
     List<Book> findByTitle(String title);
@@ -15,14 +18,28 @@ public interface CatalogUseCase {
 
     void addBook(CreateBookCommand command);
 
-    void removeById(Long id);
+    UpdateBookResponse updateBook(UpdateBookCommand command);
 
-    void updateBook();
+    void removeById(Long id);
 
     @Value
     class CreateBookCommand{
         String title;
         String author;
         Integer year;
+    }
+    @Value
+    class UpdateBookCommand {
+        Long id;
+        String title;
+        String author;
+        Integer year;
+    }
+
+    @Value
+    class UpdateBookResponse {
+        public static UpdateBookResponse SUCCESS = new UpdateBookResponse(true, emptyList());
+        boolean success;
+        List<String> errors;
     }
 }
