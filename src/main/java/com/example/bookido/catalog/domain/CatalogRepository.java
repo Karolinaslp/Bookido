03a37ce8662afd820eb3323e4ec07c6d5a824026
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-10-19 11:08:56 +0200

    add findById() method

--- src/main/java/com/example/bookido/catalog/domain/CatalogRepository.java
+++ src/main/java/com/example/bookido/catalog/domain/CatalogRepository.java
@@ -1,8 +1,13 @@
 package com.example.bookido.catalog.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CatalogRepository {
     List<Book> findAll();
+
     void save(Book book);
+
+    Optional<Book> findById(Long id);
 }
+
