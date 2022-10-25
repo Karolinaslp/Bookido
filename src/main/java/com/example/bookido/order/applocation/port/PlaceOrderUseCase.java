9c960b671decedb89194cbcd05bdaeae5d5d7c8e
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-10-25 16:44:11 +0200

    create and implement PlaceOrderResponse inner class

---
+++ src/main/java/com/example/bookido/order/applocation/port/PlaceOrderUseCase.java
@@ -0,0 +1,39 @@
+package com.example.bookido.order.applocation.port;
+
+import com.example.bookido.order.domain.OrderItem;
+import com.example.bookido.order.domain.Recipient;
+import lombok.Builder;
+import lombok.Singular;
+import lombok.Value;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+public interface PlaceOrderUseCase {
+    PlaceOrderResponse placeOrder(PlaceOrderCommand command);
+
+    @Builder
+    @Value
+    class PlaceOrderCommand {
+        @Singular
+        List<OrderItem> items;
+        Recipient recipient;
+    }
+
+    @Value
+    class PlaceOrderResponse {
+        boolean success;
+        Long orderId;
+        List<String> errors;
+
+        public static PlaceOrderResponse success(Long orderId) {
+            return new PlaceOrderResponse(true, orderId, emptyList());
+        }
+
+        public static PlaceOrderResponse failure(String... errors) {
+            return new PlaceOrderResponse(false, null, Arrays.asList(errors));
+        }
+    }
+}
