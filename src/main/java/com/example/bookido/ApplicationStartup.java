3de567703a37abfa8ae774ac3625283fb3680482
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-10-25 16:41:32 +0200

    create placeOrder() and searchCatalog() methods

--- src/main/java/com/example/bookido/ApplicationStartup.java
+++ src/main/java/com/example/bookido/ApplicationStartup.java
@@ -27,6 +27,15 @@ public class ApplicationStartup implements CommandLineRunner {
     @Override
     public void run(String... args) {
         initData();
+        searchCatalog();
+        placeOrder();
+    }
+
+    private void placeOrder() {
+
+    }
+
+    private void searchCatalog() {
         findByTitle();
         findAndUpdate();
         findByTitle();
