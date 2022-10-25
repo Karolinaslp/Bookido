8fadff2c2bb46df1fb85829ba26e3a43090ebbc2
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-10-25 16:42:13 +0200

    clean code

--- src/main/java/com/example/bookido/order/infrastructure/MemoryOrderRepository.java
+++ src/main/java/com/example/bookido/order/infrastructure/MemoryOrderRepository.java
@@ -1,32 +1,36 @@
-package com.example.bookido.order.domain;
+package com.example.bookido.order.infrastructure;
 
-import com.example.bookido.catalog.domain.Book;
+import com.example.bookido.order.domain.Order;
+import com.example.bookido.order.domain.OrderRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class MemoryOrderRepository implements OrderRepository {
     private final Map<Long, Order> storage = new ConcurrentHashMap<>();
-    private final AtomicLong ID_NEXT_VALUE = new AtomicLong(0L);
+    private final AtomicLong NEXT_ID = new AtomicLong(0L);
 
     @Override
     public Order save(Order order) {
         if (order.getId() != null) {
             storage.put(order.getId(), order);
         } else {
-            final long nextId = nextId();
+            long nextId = nextId();
             order.setId(nextId);
+            order.setCreatedAt(LocalDateTime.now());
             storage.put(nextId, order);
         }
-
         return order;
     }
 
     private long nextId() {
-        return ID_NEXT_VALUE.getAndIncrement();
+        return NEXT_ID.getAndIncrement();
     }
 
     @Override
