bf8561f2799f15d744af541b3c2352e613a96111
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-01 08:30:08 +0100

    learn about REST create getById() method

--- src/main/java/com/example/bookido/catalog/web/CatalogController.java
+++ src/main/java/com/example/bookido/catalog/web/CatalogController.java
@@ -3,9 +3,9 @@ package com.example.bookido.catalog.web;
 import com.example.bookido.catalog.application.port.CatalogUseCase;
 import com.example.bookido.catalog.domain.Book;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -17,7 +17,16 @@ class CatalogController {
     private final CatalogUseCase catalog;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Book> getAll() {
         return catalog.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getById(@PathVariable Long id) {
+        return catalog
+                .findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
