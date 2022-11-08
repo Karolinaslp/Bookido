c22d375b89a12be4747c8def9d8e7d9f28415b13
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-08 09:39:08 +0100

    make addBook() method return Book

--- src/main/java/com/example/bookido/catalog/application/port/CatalogUseCase.java
+++ src/main/java/com/example/bookido/catalog/application/port/CatalogUseCase.java
@@ -26,7 +26,7 @@ public interface CatalogUseCase {
 
     Optional<Book> findOneByTitleAndAuthor(String title, String author);
 
-    void addBook(CreateBookCommand command);
+    Book addBook(CreateBookCommand command);
 
     UpdateBookResponse updateBook(UpdateBookCommand command);
 
