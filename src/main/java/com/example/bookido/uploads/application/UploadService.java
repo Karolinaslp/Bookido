19d3da27d1cd86a613c6344fa9acaad5ca884f01
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-22 17:08:38 +0100

    create UploadService class, and save() method

---
+++ src/main/java/com/example/bookido/uploads/application/UploadService.java
@@ -0,0 +1,30 @@
+package com.example.bookido.uploads.application;
+
+import com.example.bookido.uploads.application.ports.UploadUseCase;
+import com.example.bookido.uploads.domain.Upload;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class UploadService implements UploadUseCase {
+    private final Map<String, Upload> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public Upload save(SaveUploadCommand command) {
+        String newId = RandomStringUtils.randomAlphanumeric(8);
+        Upload upload = new Upload(
+                newId,
+                command.getFile(),
+                command.getContentType(),
+                command.getFilename(),
+                LocalDateTime.now()
+        );
+        storage.put(upload.getId(), upload);
+        System.out.println("Upload save: " + upload.getFileName() + " with id: " + newId);
+        return upload;
+    }
+}
