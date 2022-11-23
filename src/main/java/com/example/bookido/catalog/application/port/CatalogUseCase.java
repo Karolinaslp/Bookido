8234b4f41b8a27bee96cecd6eb59a06b79c73969
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-23 20:59:08 +0100

    add removeBookCover() method

--- src/main/java/com/example/bookido/catalog/application/port/CatalogUseCase.java
+++ src/main/java/com/example/bookido/catalog/application/port/CatalogUseCase.java
@@ -35,6 +35,8 @@ public interface CatalogUseCase {
 
     void updateBookCover(UpdateBookCoverCommand command);
 
+    void removeBookCover(Long id);
+
     @Value
     class UpdateBookCoverCommand {
         Long id;
