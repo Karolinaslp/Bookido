52228876ac63cdb9cd537715c0310581edf4ad38
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-23 20:38:08 +0100

    add getUploadFile() method. learning how to send file to customer

---
+++ src/main/java/com/example/bookido/uploads/web/UploadsController.java
@@ -0,0 +1,63 @@
+package com.example.bookido.uploads.web;
+
+import com.example.bookido.uploads.application.ports.UploadUseCase;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/uploads")
+@AllArgsConstructor
+public class UploadsController {
+    private UploadUseCase upload;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UploadResponse> getUpload(@PathVariable String id) {
+        return upload.getById(id)
+                     .map(file -> {
+                         UploadResponse response = new UploadResponse(
+                            file.getId(),
+                            file.getContentType(),
+                            file.getFileName(),
+                            file.getCreatedAt()
+                         );
+                         return ResponseEntity.ok(response);
+                     })
+                     .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{id}/file")
+    public ResponseEntity<Resource> getUploadFile(@PathVariable String id) {
+        return upload.getById(id)
+                .map(file -> {
+                    String contentDisposition = "attachment; filename=\"" + file.getFileName() + "\"";
+                    byte[] bytes = file.getFile();
+                    Resource resource = new ByteArrayResource(bytes);
+                    return ResponseEntity
+                            .ok()
+                            .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                            .contentType(MediaType.parseMediaType(file.getContentType()))
+                            .body(resource);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @Value
+    @AllArgsConstructor
+    static class UploadResponse {
+        String id;
+        String contentType;
+        String fileName;
+        LocalDateTime createdAt;
+    }
+}
