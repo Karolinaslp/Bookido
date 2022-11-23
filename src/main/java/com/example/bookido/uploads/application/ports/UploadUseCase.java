d4dc80d9cd490c0700f75b3efb695481f326f961
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-23 20:39:01 +0100

    add getById() method

--- src/main/java/com/example/bookido/uploads/application/ports/UploadUseCase.java
+++ src/main/java/com/example/bookido/uploads/application/ports/UploadUseCase.java
@@ -4,14 +4,18 @@ import com.example.bookido.uploads.domain.Upload;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 
+import java.util.Optional;
+
 public interface UploadUseCase {
     Upload save(SaveUploadCommand command);
+
+    Optional<Upload> getById(String id);
 
     @Value
     @AllArgsConstructor
     class SaveUploadCommand {
-      String filename;
-      byte[] file;
-      String contentType;
+        String filename;
+        byte[] file;
+        String contentType;
     }
 }
