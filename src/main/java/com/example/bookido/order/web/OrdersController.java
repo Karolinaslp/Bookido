1be4802eb111b40b222effced6db569676d2e517
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-25 15:05:36 +0100

    implementation of Order REST Controller given by course creators

---
+++ src/main/java/com/example/bookido/order/web/OrdersController.java
@@ -0,0 +1,116 @@
+package com.example.bookido.order.web;
+
+
+import com.example.bookido.order.applocation.port.ManipulateOrderUseCase;
+import com.example.bookido.order.applocation.port.ManipulateOrderUseCase.PlaceOrderCommand;
+import com.example.bookido.order.applocation.port.QueryOrderUseCase;
+import com.example.bookido.order.applocation.port.QueryOrderUseCase.RichOrder;
+import com.example.bookido.order.domain.OrderItem;
+import com.example.bookido.order.domain.OrderStatus;
+import com.example.bookido.order.domain.Recipient;
+import com.example.bookido.web.CreatedURI;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.ACCEPTED;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/orders")
+class OrdersController {
+    private final ManipulateOrderUseCase manipulateOrder;
+    private final QueryOrderUseCase queryOrder;
+
+    @GetMapping
+    public List<RichOrder> getOrders() {
+        return queryOrder.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RichOrder> getOrderById(@PathVariable Long id) {
+        return queryOrder.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Object> createOrder(@RequestBody CreateOrderCommand command) {
+        return manipulateOrder
+                .placeOrder(command.toPlaceOrderCommand())
+                .handle(
+                        orderId -> ResponseEntity.created(orderUri(orderId)).build(),
+                        error -> ResponseEntity.badRequest().body(error)
+                );
+    }
+
+    URI orderUri(Long orderId) {
+        return new CreatedURI("/" + orderId).uri();
+    }
+
+    @PutMapping("/{id}/status")
+    @ResponseStatus(ACCEPTED)
+    public void updateOrderStatus(@PathVariable Long id, @RequestBody UpdateStatusCommand command) {
+        OrderStatus orderStatus = OrderStatus
+                .parseString(command.status)
+                .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST, "Unknown status: " + command.status));
+        manipulateOrder.updateOrderStatus(id, orderStatus);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable Long id) {
+        manipulateOrder.deleteOrderById(id);
+    }
+
+    @Data
+    static class CreateOrderCommand {
+        List<OrderItemCommand> items;
+        RecipientCommand recipient;
+
+        PlaceOrderCommand toPlaceOrderCommand() {
+            List<OrderItem> orderItems = items
+                    .stream()
+                    .map(item -> new OrderItem(item.bookId, item.quantity))
+                    .collect(Collectors.toList());
+            return new PlaceOrderCommand(orderItems, recipient.toRecipient());
+        }
+
+    }
+
+    @Data
+    static class OrderItemCommand {
+        Long bookId;
+        int quantity;
+    }
+
+    @Data
+    static class RecipientCommand {
+        String name;
+        String phone;
+        String street;
+        String city;
+        String zipCode;
+        String email;
+
+        Recipient toRecipient() {
+            return new Recipient(name, phone, street, city, zipCode, email);
+        }
+    }
+
+    @Data
+    static class UpdateStatusCommand {
+        String status;
+    }
+
+}
+
