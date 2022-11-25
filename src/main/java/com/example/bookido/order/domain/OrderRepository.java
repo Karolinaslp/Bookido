1d369e5058bcadb8ab6ca9cf149a7f79b346a972
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-25 15:05:22 +0100

    implementation of Order REST Controller given by course creators

--- src/main/java/com/example/bookido/order/domain/OrderRepository.java
+++ src/main/java/com/example/bookido/order/domain/OrderRepository.java
@@ -1,8 +1,14 @@
 package com.example.bookido.order.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository {
     Order save(Order order);
+
     List<Order> findAll();
+
+    Optional<Order> findById(Long id);
+
+    void deleteById(Long id);
 }
