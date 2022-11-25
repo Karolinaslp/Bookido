3f9c15d5a7ba0464303cd48612d6a2845bf9dad5
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-25 15:05:16 +0100

    implementation of Order REST Controller given by course creators

--- src/main/java/com/example/bookido/order/domain/OrderItem.java
+++ src/main/java/com/example/bookido/order/domain/OrderItem.java
@@ -1,10 +1,9 @@
 package com.example.bookido.order.domain;
 
-import com.example.bookido.catalog.domain.Book;
 import lombok.Value;
 
 @Value
 public class OrderItem {
-    Book book;
+    Long bookId;
     int quantity;
 }
