dffe9b6d6cdd7fa267bc07b93a76a9c616ae59a3
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-25 15:05:41 +0100

    implementation of Order REST Controller given by course creators

--- src/main/java/com/example/bookido/order/domain/OrderStatus.java
+++ src/main/java/com/example/bookido/order/domain/OrderStatus.java
@@ -1,5 +1,16 @@
 package com.example.bookido.order.domain;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum OrderStatus {
     NEW, CONFIRMED, IN_DELIVERY, DELIVERED, CANCELED, RETURNED;
+
+    public static Optional<OrderStatus> parseString(String value) {
+        return Arrays.stream(values())
+                .filter(it -> StringUtils.equalsAnyIgnoreCase(it.name()))
+                .findFirst();
+    }
 }
