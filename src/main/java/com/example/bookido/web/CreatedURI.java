0bf3b52482b78dec71ef4d2ecd24e7c98b3d906e
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-25 15:04:38 +0100

    implementation of Order REST Controller given by course creators

---
+++ src/main/java/com/example/bookido/web/CreatedURI.java
@@ -0,0 +1,15 @@
+package com.example.bookido.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@AllArgsConstructor
+public class CreatedURI {
+    private final String path;
+
+    public URI uri() {
+        return ServletUriComponentsBuilder.fromCurrentRequestUri().path(path).build().toUri();
+    }
+}
