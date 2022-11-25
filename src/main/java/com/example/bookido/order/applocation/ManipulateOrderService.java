c9ca87a95df165ad75a3f8bedb1442efbac92c15
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-25 15:04:50 +0100

    implementation of Order REST Controller given by course creators

---
+++ src/main/java/com/example/bookido/order/applocation/ManipulateOrderService.java
@@ -0,0 +1,39 @@
+package com.example.bookido.order.applocation;
+
+import com.example.bookido.order.applocation.port.ManipulateOrderUseCase;
+import com.example.bookido.order.domain.Order;
+import com.example.bookido.order.domain.OrderRepository;
+import com.example.bookido.order.domain.OrderStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ManipulateOrderService implements ManipulateOrderUseCase {
+    private final OrderRepository repository;
+
+    @Override
+    public PlaceOrderResponse placeOrder(PlaceOrderCommand command) {
+        Order order = Order
+                .builder()
+                .recipient(command.getRecipient())
+                .items(command.getItems())
+                .build();
+        Order save = repository.save(order);
+        return PlaceOrderResponse.success(save.getId());
+    }
+
+    @Override
+    public void deleteOrderById(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public void updateOrderStatus(Long id, OrderStatus status) {
+        repository.findById(id)
+                .ifPresent(order -> {
+                    order.setStatus(status);
+                    repository.save(order);
+                });
+    }
+}
