87d0b9936b531d167621f0957d2c9ca8c7ac9b74
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-25 15:05:48 +0100

    implementation of Order REST Controller given by course creators

--- src/main/java/com/example/bookido/order/applocation/QueryOrderService.java
+++ src/main/java/com/example/bookido/order/applocation/QueryOrderService.java
@@ -1,20 +1,58 @@
 package com.example.bookido.order.applocation;
 
+import com.example.bookido.catalog.domain.Book;
+import com.example.bookido.catalog.domain.CatalogRepository;
 import com.example.bookido.order.applocation.port.QueryOrderUseCase;
 import com.example.bookido.order.domain.Order;
+import com.example.bookido.order.domain.OrderItem;
 import com.example.bookido.order.domain.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class QueryOrderService implements QueryOrderUseCase {
-   private final OrderRepository repository;
+    private final OrderRepository repository;
+    private final CatalogRepository catalogRepository;
 
     @Override
-    public List<Order> findAll() {
-        return repository.findAll();
+    public List<RichOrder> findAll() {
+        return repository
+                .findAll()
+                .stream()
+                .map(this::toRichOrder)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<RichOrder> findById(Long id) {
+        return repository.findById(id).map(this::toRichOrder);
+    }
+
+    private RichOrder toRichOrder(Order order) {
+        List<RichOrderItem> richItems = toRichItems(order.getItems());
+        return new RichOrder(
+                order.getId(),
+                order.getStatus(),
+                richItems,
+                order.getRecipient(),
+                order.getCreatedAt()
+        );
+    }
+
+    private List<RichOrderItem> toRichItems(List<OrderItem> items) {
+        return items.stream()
+                .map(item -> {
+                    Book book = catalogRepository
+                            .findById(item.getBookId())
+                            .orElseThrow(() -> new IllegalArgumentException("Unable to find book with ID: " + item.getBookId()));
+                    return new RichOrderItem(book, item.getQuantity());
+                })
+                .collect(Collectors.toList());
+
     }
 }
