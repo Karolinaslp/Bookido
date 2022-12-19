0f4867263581fec8b4d599a5717fbfcdc2c587e2
Karolinaslp <slempkarolina@gmail.com>
2022-12-19 16:45:26 +0100

    Revert "add @Entity annotation"
    This reverts commit 7b25bb01395fb9fb445189465ec79cb5389a2ba7.

--- src/main/java/com/example/bookido/catalog/domain/Book.java
+++ src/main/java/com/example/bookido/catalog/domain/Book.java
@@ -5,29 +5,25 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
-@Entity
 public class Book {
-    @Id
-    private Long id;
-    private String title;
-    private String author;
-    private Integer year;
-    private BigDecimal price;
-    private String coverId;
+  private Long id;
+  private String title;
+  private String author;
+  private Integer year;
+  private BigDecimal price;
+  private String coverId;
 
-    public Book(String title, String author, Integer year, BigDecimal price) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-        this.price = price;
-    }
+  public Book(String title, String author, Integer year, BigDecimal price) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.price = price;
+  }
 
 }
