c436a01054a4ff8dfbb8f48a635ca436223e2d7d
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-22 19:17:50 +0100

    make Update class an entity

--- src/main/java/com/example/bookido/uploads/domain/Upload.java
+++ src/main/java/com/example/bookido/uploads/domain/Upload.java
@@ -1,14 +1,33 @@
 package com.example.bookido.uploads.domain;
 
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
-@Value
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
 public class Upload {
-    String id;
-    byte[] file;
-    String contentType;
-    String fileName;
-    LocalDateTime createdAt;
+    @Id
+    @GeneratedValue
+   private Long id;
+   private byte[] file;
+   private String contentType;
+    private String filename;
+    @CreatedDate
+   private LocalDateTime createdAt;
+
+    public Upload(String filename, String contentType, byte[] file) {
+        this.filename = filename;
+        this.contentType = contentType;
+        this.file = file;
+    }
 }
