461b318f7a4e989694b61c0ef7c7b186973fad62
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-22 19:10:13 +0100

    refactoring due to change of repository interface

--- src/main/java/com/example/bookido/order/applocation/PlaceOrderService.java
+++ src/main/java/com/example/bookido/order/applocation/PlaceOrderService.java
@@ -1,15 +1,15 @@
 package com.example.bookido.order.applocation;
 
 import com.example.bookido.order.applocation.port.PlaceOrderUseCase;
+import com.example.bookido.order.db.OrderJpaRepository;
 import com.example.bookido.order.domain.Order;
-import com.example.bookido.order.domain.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PlaceOrderService implements PlaceOrderUseCase {
-    private final OrderRepository repository;
+    private final OrderJpaRepository repository;
 
     @Override
     public PlaceOrderResponse placeOrder(PlaceOrderCommand command) {
