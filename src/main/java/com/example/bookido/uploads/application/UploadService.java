eb23fdc86744014f73ff653e9b16851994458bc8
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-22 19:19:00 +0100

    refactoring due to change of repository interface

--- src/main/java/com/example/bookido/uploads/application/UploadService.java
+++ src/main/java/com/example/bookido/uploads/application/UploadService.java
@@ -1,41 +1,38 @@
 package com.example.bookido.uploads.application;
 
 import com.example.bookido.uploads.application.ports.UploadUseCase;
+import com.example.bookido.uploads.db.UploadJpaRepository;
 import com.example.bookido.uploads.domain.Upload;
-import org.apache.commons.lang3.RandomStringUtils;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
+
 
 @Service
+@AllArgsConstructor
 public class UploadService implements UploadUseCase {
-    private final Map<String, Upload> storage = new ConcurrentHashMap<>();
+    private final UploadJpaRepository repository;
 
     @Override
     public Upload save(SaveUploadCommand command) {
-        String newId = RandomStringUtils.randomAlphanumeric(8).toLowerCase();
         Upload upload = new Upload(
-                newId,
-                command.getFile(),
-                command.getContentType(),
                 command.getFilename(),
-                LocalDateTime.now()
+                command.getContentType(),
+                command.getFile()
         );
-        storage.put(upload.getId(), upload);
-        System.out.println("Upload save: " + upload.getFileName() + " with id: " + newId);
+        repository.save(upload);
+        System.out.println("Upload save: " + upload.getFilename() + " with id: " + upload.getId());
         return upload;
     }
 
     @Override
-    public Optional<Upload> getById(String id) {
-        return Optional.ofNullable(storage.get(id));
+    public Optional<Upload> getById(Long id) {
+        return repository.findById(id);
     }
 
     @Override
-    public void removeById(String id) {
-        storage.remove(id);
+    public void removeById(Long id) {
+        repository.deleteById(id);
     }
 }
