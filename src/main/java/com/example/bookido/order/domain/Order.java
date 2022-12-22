0884177618f1d207654f237d81d53474acbf826f
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-22 19:12:12 +0100

    make Order class an entity

--- src/main/java/com/example/bookido/order/domain/Order.java
+++ src/main/java/com/example/bookido/order/domain/Order.java
@@ -1,25 +1,42 @@
 package com.example.bookido.order.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.auditing.AuditableBeanWrapper;
 
-import java.math.BigDecimal;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+@EntityListeners(AuditableBeanWrapper.class)
 public class Order {
-
+    @Id
+    @GeneratedValue
     private Long id;
 
-   @Builder.Default
-   private OrderStatus status = OrderStatus.NEW;
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status = OrderStatus.NEW;
 
-   private List<OrderItem> items;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> items;
 
-   private Recipient recipient;
+    private transient Recipient recipient;
 
-   private LocalDateTime createdAt;
+    @CreatedDate
+    private LocalDateTime createdAt;
 
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
