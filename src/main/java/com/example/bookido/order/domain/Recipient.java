d464aa20c65f11ce8e847c8c28ba0e4b753497e9
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-22 19:17:18 +0100

    make all fields private

--- src/main/java/com/example/bookido/order/domain/Recipient.java
+++ src/main/java/com/example/bookido/order/domain/Recipient.java
@@ -1,6 +1,9 @@
 package com.example.bookido.order.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
