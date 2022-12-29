958c8be1e3c00a85400c274bede7b067aeddb93c
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-29 20:59:45 +0100

    change author field to Set of authors ids. move toBook() method to CatalogService

--- src/main/java/com/example/bookido/catalog/application/port/CatalogUseCase.java
+++ src/main/java/com/example/bookido/catalog/application/port/CatalogUseCase.java
@@ -9,6 +9,7 @@ import lombok.Value;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static java.util.Collections.emptyList;
 
@@ -48,13 +49,9 @@ public interface CatalogUseCase {
     @Value
     class CreateBookCommand {
         String title;
-        String author;
+        Set<Long> authors;
         Integer year;
         BigDecimal price;
-
-        public Book toBook() {
-            return new Book(title, author, year, price);
-        }
     }
 
     @Value
@@ -63,24 +60,9 @@ public interface CatalogUseCase {
     class UpdateBookCommand {
         Long id;
         String title;
-        String author;
+        Set<Long> authors;
         Integer year;
         BigDecimal price;
-        public Book updateFields(Book book) {
-            if (title != null) {
-                book.setTitle(title);
-            }
-            if (author != null) {
-                book.setAuthor(author);
-            }
-            if (year != null) {
-                book.setYear(year);
-            }
-            if (price != null) {
-                book.setPrice(price);
-            }
-            return book;
-        }
     }
 
     @Value
