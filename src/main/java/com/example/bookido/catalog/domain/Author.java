7af7547cbb70a62ac8ce98d45af433e13c22d212
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-29 20:38:59 +0100

    add @JsonIgnoreProperties annotation to fix stuckOverflow error

--- src/main/java/com/example/bookido/catalog/domain/Author.java
+++ src/main/java/com/example/bookido/catalog/domain/Author.java
@@ -1,6 +1,10 @@
 package com.example.bookido.catalog.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -11,6 +15,8 @@ import java.util.Set;
 @Data
 @Entity
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@ToString(exclude = "books")
 public class Author {
     @Id
     @GeneratedValue
@@ -21,8 +27,14 @@ public class Author {
     private String lastName;
 
     @ManyToMany(fetch = FetchType.EAGER, mappedBy = "authors")
+    @JsonIgnoreProperties("authors")
     private Set<Book> books;
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
