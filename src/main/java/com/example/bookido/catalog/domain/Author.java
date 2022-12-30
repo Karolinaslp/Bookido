0b1a7496e9f49b7369765c56b2df9d5ee6a87f62
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-30 14:10:25 +0100

    clean imports

--- src/main/java/com/example/bookido/catalog/domain/Author.java
+++ src/main/java/com/example/bookido/catalog/domain/Author.java
@@ -1,6 +1,5 @@
 package com.example.bookido.catalog.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
