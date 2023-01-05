3da495eec000da61e60fe50a9f12cbe519513868
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-01-05 16:51:30 +0100

    added version field with @Version annotation to avoid two process  to modify the same entity in the same time

--- src/main/java/com/example/bookido/jpa/BaseEntity.java
+++ src/main/java/com/example/bookido/jpa/BaseEntity.java
@@ -7,16 +7,19 @@ import lombok.Setter;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
 import java.util.UUID;
 
 @MappedSuperclass
 @Getter
 @Setter
-@EqualsAndHashCode
+@EqualsAndHashCode(of = "uuid")
 public abstract class BaseEntity {
     @Id
     @GeneratedValue
     private Long id;
 
     private String uuid = UUID.randomUUID().toString();
+    @Version
+    private long version;
 }
