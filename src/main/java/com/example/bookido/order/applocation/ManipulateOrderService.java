0270551681c0b658d5c328dfa4d4c0624a12ecc4
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-01-10 16:59:00 +0100

    changed placeOrder() now method validate if is enough quantity of book to make an order

--- src/main/java/com/example/bookido/order/applocation/ManipulateOrderService.java
+++ src/main/java/com/example/bookido/order/applocation/ManipulateOrderService.java
@@ -1,26 +1,62 @@
 package com.example.bookido.order.applocation;
 
+import com.example.bookido.catalog.db.BookJpaRepository;
+import com.example.bookido.catalog.domain.Book;
 import com.example.bookido.order.applocation.port.ManipulateOrderUseCase;
 import com.example.bookido.order.db.OrderJpaRepository;
 import com.example.bookido.order.domain.Order;
+import com.example.bookido.order.domain.OrderItem;
 import com.example.bookido.order.domain.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ManipulateOrderService implements ManipulateOrderUseCase {
     private final OrderJpaRepository repository;
+    private final BookJpaRepository bookJpaRepository;
 
     @Override
     public PlaceOrderResponse placeOrder(PlaceOrderCommand command) {
+        Set<OrderItem> items = command
+                .getItems()
+                .stream()
+                .map(this::toOrderItem)
+                .collect(Collectors.toSet());
+
         Order order = Order
                 .builder()
                 .recipient(command.getRecipient())
-                .items(command.getItems())
+                .items(items)
                 .build();
         Order save = repository.save(order);
+        bookJpaRepository.saveAll(updateBooks(items));
         return PlaceOrderResponse.success(save.getId());
+    }
+
+    private Set<Book> updateBooks(Set<OrderItem> items) {
+        return items
+                .stream()
+                .map(item -> {
+                    Book book = item.getBook();
+                    book.setAvailable(book.getAvailable() - item.getQuantity());
+                    return book;
+                })
+                .collect(Collectors.toSet());
+    }
+
+    private OrderItem toOrderItem(OrderItemCommand command) {
+        Book book = bookJpaRepository.getOne(command.getBookId());
+        int quantity = command.getQuantity();
+        if (book.getAvailable() >= quantity) {
+            return new OrderItem(book, quantity);
+        }
+        throw new IllegalArgumentException("Too many copies of book " + book.getId() + " requested: " + quantity + " of " + book.getAvailable() + " available ");
     }
 
     @Override
