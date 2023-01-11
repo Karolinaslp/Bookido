b79a77b90140a5f1580ed8e1417bbd80b9292710
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-01-11 17:47:03 +0100

    added method to verify for putting back to storage books if order is abandoned or canceled

--- src/main/java/com/example/bookido/order/applocation/ManipulateOrderService.java
+++ src/main/java/com/example/bookido/order/applocation/ManipulateOrderService.java
@@ -5,10 +5,7 @@ import com.example.bookido.catalog.domain.Book;
 import com.example.bookido.order.applocation.port.ManipulateOrderUseCase;
 import com.example.bookido.order.db.OrderJpaRepository;
 import com.example.bookido.order.db.RecipientJpaRepository;
-import com.example.bookido.order.domain.Order;
-import com.example.bookido.order.domain.OrderItem;
-import com.example.bookido.order.domain.OrderStatus;
-import com.example.bookido.order.domain.Recipient;
+import com.example.bookido.order.domain.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,7 +35,7 @@ public class ManipulateOrderService implements ManipulateOrderUseCase {
                 .items(items)
                 .build();
         Order save = repository.save(order);
-        bookJpaRepository.saveAll(updateBooks(items));
+        bookJpaRepository.saveAll(revokeBooks(items));
         return PlaceOrderResponse.success(save.getId());
     }
 
@@ -49,7 +46,7 @@ public class ManipulateOrderService implements ManipulateOrderUseCase {
 
     }
 
-    private Set<Book> updateBooks(Set<OrderItem> items) {
+    private Set<Book> reduceBooks(Set<OrderItem> items) {
         return items
                 .stream()
                 .map(item -> {
@@ -78,8 +75,22 @@ public class ManipulateOrderService implements ManipulateOrderUseCase {
     public void updateOrderStatus(Long id, OrderStatus status) {
         repository.findById(id)
                 .ifPresent(order -> {
-                    order.updateStatus(status);
+                    UpdateStatusResult result = order.updateStatus(status);
+                    if (result.isRevoked()) {
+                        bookJpaRepository.saveAll(revokeBooks(order.getItems()));
+                    }
                     repository.save(order);
                 });
+    }
+
+    private Set<Book> revokeBooks(Set<OrderItem> items) {
+        return items
+                .stream()
+                .map(item -> {
+                    Book book = item.getBook();
+                    book.setAvailable(book.getAvailable() + item.getQuantity());
+                    return book;
+                })
+                .collect(Collectors.toSet());
     }
 }
