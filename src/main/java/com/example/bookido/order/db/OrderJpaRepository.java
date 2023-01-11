6a40791fbf6267071348e5bc20973ddde62ab7e2
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-01-11 19:47:52 +0100

    created method for finding orders with new status that have a creation date less than a specific date.

--- src/main/java/com/example/bookido/order/db/OrderJpaRepository.java
+++ src/main/java/com/example/bookido/order/db/OrderJpaRepository.java
@@ -1,8 +1,13 @@
 package com.example.bookido.order.db;
 
 import com.example.bookido.order.domain.Order;
+import com.example.bookido.order.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrderJpaRepository extends JpaRepository<Order, Long> {
+    List<Order> findByStatusAndCreatedAtLessThanEqual(OrderStatus status, LocalDateTime timestamp);
 
 }
