d832986d802566e12bc2c3100039def33ab6cf01
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-01-23 19:36:12 +0100

    added properties class for managing verification of abandoned orders

---
+++ src/main/java/com/example/bookido/order/application/OrdersProperties.java
@@ -0,0 +1,16 @@
+package com.example.bookido.order.application;
+
+import lombok.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.time.Duration;
+
+@Value
+@ConstructorBinding
+@ConfigurationProperties("app.orders")
+public
+class OrdersProperties {
+    Duration paymentPeriod;
+    String abandonCron;
+}
