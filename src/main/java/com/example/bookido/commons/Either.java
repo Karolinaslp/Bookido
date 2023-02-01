38f45a1e9ed6f33c77625de3a1a43a5a75882d8a
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-02-01 20:38:17 +0100

    added @Getter annotation

--- src/main/java/com/example/bookido/commons/Either.java
+++ src/main/java/com/example/bookido/commons/Either.java
@@ -1,9 +1,11 @@
 package com.example.bookido.commons;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.function.Function;
 
+@Getter
 @AllArgsConstructor
 public class Either<L, R> {
     private final boolean success;
