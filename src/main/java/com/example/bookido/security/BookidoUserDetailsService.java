17bae35f51ea7088e7498ef8ea50c98a8a7a7d38
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-02-10 16:56:49 +0100

    implementing security: adding admin

--- src/main/java/com/example/bookido/security/BookidoUserDetailsService.java
+++ src/main/java/com/example/bookido/security/BookidoUserDetailsService.java
@@ -9,8 +9,12 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 @AllArgsConstructor
 public class BookidoUserDetailsService implements UserDetailsService {
     private final UserEntityRepository repository;
+    private final AdminConfig config;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (config.getUsername().equalsIgnoreCase(username)) {
+            return config.adminUser();
+        }
         return repository.findByUsernameIgnoreCase(username)
                 .map(UserEntityDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
