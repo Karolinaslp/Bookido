fec0010ae0d07da35c6ee96a2cfa2bbfc152c016
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-02-13 21:58:45 +0100

    correct test

--- src/test/java/com/example/bookido/catalog/web/CatalogControllerWebTest.java
+++ src/test/java/com/example/bookido/catalog/web/CatalogControllerWebTest.java
@@ -7,6 +7,8 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -19,7 +21,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest({CatalogController.class})
+@ActiveProfiles("test")
+@WithMockUser
 class CatalogControllerWebTest {
+
     @MockBean
     CatalogUseCase catalogUseCase;
 
